3e95d19c09175daf0201013e2e30dfd91018c6cd
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-07-26 11:35:00 +0300

    Person class added for examples.

---
+++ src/main/kotlin/CommonClasses.kt
@@ -0,0 +1,15 @@
+package org.example
+
+class Person(val name: String, var age: Int = 22, var city: String = "Paris") {
+    fun moveTo(newCity: String) {
+        this.city = newCity
+    }
+
+    fun incrementAge() {
+        this.age++
+    }
+
+    override fun toString(): String {
+        return "Name: $name, age: $age, city: $city"
+    }
+}
