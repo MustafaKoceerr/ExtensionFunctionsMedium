f7a0e662ea566aed70637dd1e8031a77f2cd6a23
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-07-26 11:45:17 +0300

    Some explanations and basic information added to Main.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package org.example
 
+import kotlin.random.Random
+
 /**
  *  * Kotlin standart kütüphanesi, nesne bağlamında bir kod bloğu çalıştırma amacına sahip birkaç fonksiyon içerir.
  *  Obje üzerinde lambda bir fonksiyon çalıştırmak, geçici bir scope oluşturur.
@@ -95,5 +97,125 @@ fun main() {
     // it
     str.let {
         println("The string's lenght : ${it.length}")
+    }
+
+
+    /**
+     * THIS:
+     *
+     * run, with ve apply objeyi default olarak this ile temsil eder.
+     * Nesne, sıradan sınıf fonksiyonlarında olduğu gibi kullanılabilir durumdadır.
+     *
+     * Kodu daha kısa yapmak için this keyword'ünü çıkartabilirsin, kullanmayabilirsin.
+     * Diğer bir yandan this kullanmamak, çağırdığımız objenin mi yoksa,
+     * dışarıdan gelen bir değişkenin veya fonksiyonun çağrımımı bunu ayırt etmemizi zorlaştırır
+     *
+     * This kullanan bir fonksiyon seçilmesi;
+     * genellikle nesnenin işlevlerini çağırarak veya özelliklerine değer atayarak nesnenin üyeleri üzerinde çalışan lambda ifadeleri için önerilir.
+     */
+
+    var externalVariable = 10
+    val adam = Person("Adam").apply {
+        externalVariable = 20
+        externalFunction()
+
+        age = 20
+        city = "London"
+        incrementAge()
+    }
+    println(adam)
+
+    /**
+     * IT:
+     * let ve also, lambda fonksiyonun içinde referans default değer olarak it'i sağlar.
+     * It, this gibi örtük olarak obje'ye ulaşım sağlamaz, açıkça it.property olarak belirtmek zorundasın.
+     *
+     * Bu nedenle, nesnenin genellikle işlev çağrılarında bir argüman olarak kullanıldığı durumlarda, bağlam nesnesine it ile erişmek daha iyidir.
+     *
+     * Ayrıca birden çok değişkenin kullanıldığı durumda it'in kullanımı daha iyidir.
+     */
+
+    /**
+     * RETURN VALUE:
+     * Scope functions'lar döndürdükleri sonuçlar ile de ayrılırlar.
+     * apply ve also, context objenin kendisini döndürür
+     * let, run, with lambda sonucunu yani son satırı döndürür.
+     *
+     * Birini kullanmadan önce neyi döndürmek istediğini dikkatlice düşünmelisin, bu hangi scope function'u kullanacağını seçmekte sana yardımcı olacaktır.
+     */
+
+    /**
+     * Context Object:
+     * Apply ve also'nun döndürdüğü şey, context objenin kendisidir.
+     * Bu nedenle zincir çağrımlarda rahatlıkla kullanabilirsin.
+     *
+     * Aynı obje üzerinde zincirleme fonksiyonları sürdürebilirsin.
+     * Ayrıca fonksiyonlarda return object olarak da kullanılabilirler: Örn getRandomInt
+     */
+
+    val numberList = mutableListOf<Double>()
+    numberList.also { println("Populating the list") }
+        .apply {
+            add(2.71)
+            add(3.14)
+            add(22.0)
+        }
+        .also { println("Sorting The List") }
+        .sort()
+
+    // Ayrıca fonksiyonlar return object olarak da kullanılabilirler
+    val randomNumber = getRandomInt()
+    println(randomNumber)
+
+    /**
+     * Lambda Result:
+     * let, run ve with lambda result döndürürler. Böylece değişkenlere döndürdükleri sonuçları atayabilirsin.
+     *
+     * Objenin kendisi değil de Result döndürüp onunla zincirleme işlem yapacağında kullanabilirsin.
+     *
+     * Ayrıca return value'yu ignorelayıp, fonksiyon içinde local değişken tanımlayabilirsin.
+     */
+
+    val numbers = mutableListOf("one", "two", "three")
+    val countEndsWithE = numbers.run {
+        add("four")
+        add("five")
+        count{
+            it.endsWith("e")
+        }
+    }
+    println("There are $countEndsWithE elements that end with e.")
+
+    //     Ayrıca return value'yu ignorelayıp, fonksiyon içinde local değişken tanımlayabilirsin.
+    with(numbers) {
+        val firstItem = first()
+        val lastItem = last()
+        println("First item $firstItem, last item $lastItem")
+    }
+
+
+    /**
+     * FUNCTIONS:
+     * Doğru scope fonksiyonunu seçmen için detayını tarif edeceğiz ve önerdiğimiz kullanımları örneklerle sağlayacağız.
+     * Teknik olarak scope fonksiyonlar değişken olarak kullanılabilirler.
+     * Örneklerde alışıla gelmiş kullanımlarını göreceksiniz.
+     *
+     * Bu kullanımlar için diğer dosyalara göz atınız.
+     */
+}
+
+private fun externalFunction() {
+    println("Hello I am an external function!")
+}
+
+private fun getRandomInt(): Int {
+    return Random.nextInt(100).also {
+        println("getRandomInt generated : $it")
+    }
+}
+
+private fun getRandomInt2(): Int {
+    return Random.nextInt(100).also { value ->
+        println("getRandomInt generated : $value")
     }
 }
